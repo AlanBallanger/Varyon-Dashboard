9e18c5c615bb9d9203270b4f65c7a2ac1ded7452
alanb <alan.ballanger@live.fr>
2026-07-29 05:01:11 +0200

    handle better auto scrolling

--- src/components/layout/AppShell.vue
+++ src/components/layout/AppShell.vue
@@ -1,9 +1,9 @@
 <template>
   <div class="flex min-h-screen bg-base-100 text-base-content">
     <Sidebar />
-    <div class="flex-1 flex flex-col min-w-0">
+    <div class="flex-1 flex flex-col min-w-0 h-screen">
       <StatusBar />
-      <main class="flex-1 overflow-auto">
+      <main class="flex-1 min-h-0 overflow-hidden">
         <slot />
       </main>
     </div>
